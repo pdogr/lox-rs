f7a41ac88792a304ad6b6056f6ebdf3059ec38f1
pdogr <pawan.dogra99@gmail.com>
2022-03-09 03:44:19 +0530

    Add tests from the original lox/interpreter test suite. There are currently not ported to work with this impl.

---
+++ tests/integration_tests.rs
@@ -0,0 +1,98 @@
+use simple_test_case::dir_cases;
+use std::error::Error;
+use std::result::Result;
+use std::{env, process::Command};
+
+macro_rules! regex {
+    ($re:literal $(,)?) => {{
+        static RE: once_cell::sync::OnceCell<regex::Regex> = once_cell::sync::OnceCell::new();
+        RE.get_or_init(|| regex::Regex::new($re).unwrap())
+    }};
+}
+
+fn extract_expected_data(line_num: usize, line: &str) -> Option<String> {
+    if let Some(cap) = regex!(r"// expect: ?(.*)").captures_iter(line).next() {
+        let capture = &cap[1];
+        return Some(capture.to_string());
+    }
+
+    if let Some(cap) = regex!(r"// (Error.*)").captures_iter(line).next() {
+        let capture = &cap[1];
+        return Some(format!("[line {line_num}] {capture}"));
+    }
+
+    if let Some(cap) = regex!(r"// \[((java|c) )?line (\d+)\] (Error.*)")
+        .captures_iter(line)
+        .next()
+    {
+        if let Some("c") = cap.get(2).map(|m| m.as_str()) {
+            return None;
+        }
+        let line_num = &cap[3];
+        let capture = &cap[4];
+        return Some(format!("[line {line_num}] {capture}"));
+    }
+
+    if let Some(cap) = regex!(r"// expect runtime error: (.+)")
+        .captures_iter(line)
+        .next()
+    {
+        let capture = &cap[1];
+        return Some(format!("{capture}\n[line {line_num}]"));
+    }
+
+    if let Some(cap) = regex!(r"\[.*line (\d+)\] (Error.+)")
+        .captures_iter(line)
+        .next()
+    {
+        let line_num = &cap[1];
+        let capture = &cap[2];
+        return Some(format!("[line {line_num}] {capture}"));
+    }
+
+    if let Some(cap) = regex!(r"(\[line \d+\])").captures_iter(line).next() {
+        let capture = &cap[1];
+        return Some(capture.to_string());
+    }
+
+    None
+}
+
+fn run_test(bin_path: &str, source_file: &str, source: &str) -> Result<(), Box<dyn Error>> {
+    println!("{bin_path:?}");
+    let mut expected = String::new();
+    for (line_idx, line) in source.lines().enumerate() {
+        let line_num = line_idx + 1;
+        if let Some(line) = extract_expected_data(line_num, line) {
+            dbg!(bin_path, source_file, &line);
+            expected.push_str(&format!("{line}\n"));
+        }
+    }
+
+    let output = Command::new(bin_path)
+        .arg(&format!("../{source_file}"))
+        .output()?;
+
+    let output = String::from_utf8(output.stdout)?;
+
+    dbg!(&output, &expected);
+    assert_eq!(output, expected);
+
+    Ok(())
+}
+
+#[dir_cases("data/operator")]
+#[test]
+fn crafting_interpreters_test_suite(path: &str, contents: &str) -> Result<(), Box<dyn Error>> {
+    if path.ends_with("decimal_point_at_eof.lox")
+        || path.ends_with("equals_class.lox")
+        || path.ends_with("equals_method.lox")
+    {
+        return Ok(());
+    }
+
+    let name = env::var("CARGO_PKG_NAME")?;
+    let manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
+    let bin_path = format!("{manifest_dir}/../target/debug/{name}");
+    run_test(&bin_path, path, contents)
+}
